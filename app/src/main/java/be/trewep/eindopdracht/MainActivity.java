ed19c8a0917bbb5fc17d6e79a425a07c29eca3b1
Trewep <56358560+Trewep@users.noreply.github.com>
2021-06-02 10:00:36 +0200

    created fragments

--- app/src/main/java/be/trewep/eindopdracht/MainActivity.java
+++ app/src/main/java/be/trewep/eindopdracht/MainActivity.java
@@ -14,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     Fragment currentLayout; //holds the currents fragment that's been showed on this moment
     final ronde1Speler1 ronde1Speler1 = new ronde1Speler1();
     final ronde1Speler2 ronde1Speler2 = new ronde1Speler2();
+    final ronde1results ronde1results = new ronde1results();
+
+    final ronde2Speler1 ronde2Speler1 = new ronde2Speler1();
+    final ronde2Speler2 ronde2Speler2 = new ronde2Speler2();
+    final ronde2results ronde2results = new ronde2results();
+
+    final ronde3Speler1 ronde3Speler1 = new ronde3Speler1();
+    final ronde3Speler2 ronde3Speler2 = new ronde3Speler2();
+    final ronde3results ronde3results = new ronde3results();
+
+    final gameresults gameresults = new gameresults();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +46,23 @@ public class MainActivity extends AppCompatActivity {
 
                 if (currentLayout == ronde1Speler1) {
                     currentLayout = ronde1Speler2;
-
-
-                    //currentLayout = ronde1results;
-                    //currentLayout = ronde2Speler1;
-                    //currentLayout = ronde2Speler2;
-                    //currentLayout = ronde2results;
-                    //currentLayout = ronde3Speler1;
-                    //currentLayout = ronde3Speler2;
-                    //currentLayout = ronde3results;
-                    //currentLayout = gameresults;
-                } else {
+                }else if (currentLayout == ronde1Speler2){
+                    currentLayout = ronde1results;
+                }else if (currentLayout == ronde1results){
+                 currentLayout = ronde2Speler1 ;
+                }else if (currentLayout == ronde2Speler1){
+                    currentLayout = ronde2Speler2 ;
+                }else if (currentLayout == ronde2Speler2){
+                    currentLayout = ronde2results;
+                }else if (currentLayout == ronde2results){
+                    currentLayout = ronde3Speler1 ;
+                }else if (currentLayout == ronde3Speler1){
+                    currentLayout = ronde3Speler2 ;
+                }else if (currentLayout == ronde3Speler2){
+                    currentLayout = ronde3results;
+                }else if (currentLayout == ronde3results){
+                    currentLayout = gameresults ;
+                }else {
                     currentLayout = ronde1Speler1;
                 }
 
---
+++ app/src/main/java/be/trewep/eindopdracht/ronde2Speler2.java
@@ -0,0 +1,62 @@
+package be.trewep.eindopdracht;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class ronde2Speler2 extends Fragment {
+    TextView tvLetter;
+    GameViewModel viewModel;
+    Button btnVowel;
+    Button btnConsonant;
+
+    public ronde2Speler2() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_ronde1_speler1, container, false);
+
+        tvLetter = v.findViewById(R.id.tv_letter);
+
+        btnVowel = v.findViewById(R.id.btn_vowel);
+        btnConsonant = v.findViewById(R.id.btn_consonant);
+
+        btnVowel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                viewModel.pickVowel();
+            }
+        });
+
+        btnConsonant.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                viewModel.pickConsonant();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(GameViewModel.class);
+
+        viewModel.getChar().observe(this, letter -> {
+            tvLetter.append(letter.toString() + " ");
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        viewModel.clearCounter();
+    }
+
+}
