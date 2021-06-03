97cd86f5a1cdd72c188420f5f18ec486f8ca29e4
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-06-04 00:14:29 +0200

    build: end-result display

--- app/src/main/java/be/trewep/eindopdracht/gameresults.java
+++ app/src/main/java/be/trewep/eindopdracht/gameresults.java
@@ -4,60 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link gameresults#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class gameresults extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    GameViewModel viewModel;
+    //Result
+    TextView mTextViewResultR1S1;
+    TextView mTextViewResultR1S2;
+    TextView mTextViewResultR2S1;
+    TextView mTextViewResultR2S2;
+    TextView mTextViewResultR3S1;
+    TextView mTextViewResultR3S2;
 
     public gameresults() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ronde1results.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static gameresults newInstance(String param1, String param2) {
-        gameresults fragment = new gameresults();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
+    //Result
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ronde1results, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_gameresults, container, false);
+        viewModel = new ViewModelProvider(requireActivity()).get(GameViewModel.class);
+
+        mTextViewResultR1S1 = v.findViewById(R.id.tVAntwoordR1S1);
+        mTextViewResultR1S2 = v.findViewById(R.id.tVAntwoordR1S2);
+        mTextViewResultR2S1 = v.findViewById(R.id.tVAntwoordR2S1);
+        mTextViewResultR2S2 = v.findViewById(R.id.tVAntwoordR2S2);
+        mTextViewResultR3S1 = v.findViewById(R.id.tVAntwoordR3S1);
+        mTextViewResultR3S2 = v.findViewById(R.id.tVAntwoordR3S2);
+
+        mTextViewResultR1S1.setText(viewModel.string1);
+        mTextViewResultR1S2.setText(viewModel.string2);
+        mTextViewResultR2S1.setText(viewModel.string3);
+        mTextViewResultR2S2.setText(viewModel.string4);
+        mTextViewResultR3S1.setText(viewModel.int1.toString());
+        mTextViewResultR3S2.setText(viewModel.int2.toString());
+        return v;
     }
 }
