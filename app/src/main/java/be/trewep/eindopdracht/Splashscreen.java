baf325cc42d3a57c6c52a4a141b48462b189fed7
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-06-03 19:08:13 +0200

    add: Splashscreen and logo

---
+++ app/src/main/java/be/trewep/eindopdracht/Splashscreen.java
@@ -0,0 +1,26 @@
+package be.trewep.eindopdracht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+    int DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },DELAY);
+    }
+}
